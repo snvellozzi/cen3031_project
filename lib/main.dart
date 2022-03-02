b57475974ba42c94e30c23bfaaccd58b806efe0b
James <35617601+jliby@users.noreply.github.com>
2022-03-02 15:47:21 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,161 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:scrummy_bears/model/Question.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MaterialApp(
+    home:QuizApp(),
+  ),);
+}
+class QuizApp extends StatefulWidget {
+  @override
+  _QuizAppState createState() => _QuizAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _QuizAppState extends State<QuizApp> {
+  int _currentQuestionIndex = 0;
 
-  // This widget is the root of your application.
+  List questionBank = [
+    Question.name(
+        "Do you prefer working a group setting or independently?",  ["I prefer to work in as group setting", "I prefer to work independently"]),
+    Question.name("Do you like animals?", ["Yes I love animal", "I prefer not to work with animals"]),
+    
+    Question.name("Is career salary important?", ["Yes, I find career salary very important.", "No, I career salary is not important"]),
+
+    Question.name("Do you like politics?", ["Yes, I find politics interesting.", "I, do not find politics interesting."]),
+
+    Question.name("Do you like technology?", ["Yes, I love technology, I would to work with politcis.", "I do not like politics."]),
+
+
+
+  
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Scrummy Bears",
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.orange,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Major Matcher Quiz"),
+        centerTitle: true,
+       // backgroundColor: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Welcome to Scrummy'),
+
+     // backgroundColor: Colors.blueGrey,
+
+      // We use [Builder] here to use a [context] that is a descendant of [Scaffold]
+      //or else [Scaffold.of] will return null
+      body: Builder(
+        builder: (BuildContext context) => Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+          
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(14.4),
+                      border: Border.all(
+                          color: Colors.blueGrey.shade400,
+                          style: BorderStyle.solid)),
+                  height: 120.0,
+                  child: Center(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      questionBank[_currentQuestionIndex].questionText,
+//                      style: TextStyle(fontSize: 16.9,
+//                          color: Colors.white),
+                    ),
+                  )),
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                
+                children: <Widget>[
+                  
+                  RaisedButton(
+                    onPressed: () => _prevQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(true, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      questionBank[_currentQuestionIndex].getAnswer(0),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(false, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      questionBank[_currentQuestionIndex].getAnswer(1),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _nextQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  _prevQuestion() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentQuestionIndex = (_currentQuestionIndex - 1) % questionBank.length;
+      debugPrint("Index: $_currentQuestionIndex");
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  _checkAnswer(bool userChoice, BuildContext context) {
+    if (true) {
+      //correct answer
+      // final snackbar = SnackBar(
+      //     backgroundColor: Colors.green,
+      //     duration: Duration(milliseconds: 500),
+      //     content: Text("Correct!"));
+      // Scaffold.of(context).showSnackBar(snackbar);
+      _updateQuestion();
+    } else {
+      // debugPrint("Incorrect!");
+      // final snackbar = SnackBar(
+      //     backgroundColor: Colors.redAccent,
+      //     duration: Duration(milliseconds: 500),
+      //     content: Text("Incorrect!"));
+      // Scaffold.of(context).showSnackBar(snackbar);
+
+      _updateQuestion();
+    }
+  }
+
+  _updateQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % questionBank.length;
+    });
+  }
+
+  _nextQuestion() {
+    _updateQuestion();
   }
 }
