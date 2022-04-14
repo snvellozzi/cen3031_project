3c7ae01f2e63f8bb49efeaea379caf3cfb737c6a
Sophia Vellozzi <snvello@gmail.com>
2022-04-13 21:18:30 -0400

    careers dart file

---
+++ lib/screens/careers.dart
@@ -0,0 +1,120 @@
+class Careers {
+
+  static List<Careers> getCareers() =>  [
+    Careers("Computer Science", "Software Engineer", "A software engineer is a person who applies the principles of software engineering to design, develop, maintain, test, and evaluate computer software.",
+            "\$108,249", "Bachelor's (4 years)", "Analyse user requirements; write and test code; research, design and write new software programs; evaluate the software and systems that make computers and hardware work."),
+
+    Careers("Computer Science", "Product Manager", "A product manager is the person who identifies the customer need and the larger business objectives that a product or feature will fulfill, articulates what success looks like for a product, and rallies a team to turn that vision into a reality.",
+        "\$113,446", "Bachelor's (4 years)", "Define the product strategy and roadmap; work with external third parties to assess partnerships and licensing opportunities; be an expert with respect to the competition; act as a leader within the company."),
+
+    Careers("Computer Science", "Data Scientist", "Data scientists utilize their analytical, statistical, and programming skills to collect, analyze, and interpret large data sets.",
+        "\$100,560", "Master's or Ph.D.", "Ask the right questions to begin the discovery process; acquire data; process and clean the data; integrate and store data."),
+
+    Careers("Journalism", "News Journalist", "A Journalist, or Reporter, is responsible for researching and writing informational news articles and stories about real events using a fair and unbiased perspective.",
+        "\$23,733 per year", "Bachelor's (4 years)", "Interviewing experts; gathering first-hand accounts of events and organizing an outline into a cohesive, interesting story."),
+
+    Careers("Journalism", "Staff Writer", "The staff writer's responsibilities include researching topics, interviewing relevant individuals, creating content, and editing your own or other writers' work.",
+        "\$49,556 per year", "Bachelor's (4 years)", "Performing research on different topics; analyzing and interpreting findings by breaking down data; uncovering newsworthy stories."),
+
+    Careers("Journalism", "Communications Manager", "Communications Managers create, implement and oversee communications programs, be it internal or external, that effectively describe and promote the organization and its products.",
+        "\$112,065 per year", "Bachelor's (4 years)", "Preparing detailed media reports, press releases, and marketing materials."),
+
+    Careers("Biology", "Biologist", "Biologists study organisms and plant life to learn more about their composition, behaviors, habitats, and how they interact with other organisms and their environment.",
+        "\$66,975 per year", "Bachelor's (4 years) or Master's", "Conduct research; collect samples and measurements; perform tests and experiments; interpret and report their findings."),
+
+    Careers("Biology", "Research Scientist", "Research scientists are responsible for designing, undertaking and analysing information from controlled laboratory-based investigations, experiments and trials.",
+        "\$97,507 per year", "Master's or Ph.D.", "Creating research proposals; planning and conducting experiments; collecting samples; monitoring experiments."),
+
+    Careers("Biology", "Biology Teacher", "The biology teacher's responsibilities include preparing lesson plans, tests and assignments, supervising students during investigations and experiments, and acting as a field trip chaperone.",
+        "\$55,094 per year", "Bachelor's (4 years)", "Develop and initiate biology teaching materials and course content; deliver lectures to students on living organisms and things; teach students on how cells function in relation to their environments; teach and encourage students to conduct experiments in biology."),
+
+    Careers("Business", "Accountant", "An Accountant helps businesses make critical financial decisions by collecting, tracking, and correcting the company's finances.",
+        "\$53,973 per year", "Bachelor's (4 years)", "Perform financial audits; reconciling bank statements; ensuring financial records are accurate throughout the year."),
+
+    Careers("Business", "Investment Banker", "An investment banker is responsible for raising capital for businesses and individuals by issuing debt and selling equity.",
+        "\$64,520 per year", "Bachelor's (4 years)", "Conduct research and due diligence investigations to understand the market; develop strategies to discover potential investors; analyze risk and offer financial advice."),
+
+    Careers("Business", "Financial Advisor", "Personal financial advisors assess the financial needs of individuals and help them with decisions on investments (such as stocks and bonds), tax laws, and insurance.",
+        "\$65,278 per year", "Bachelor's (4 years)", "Market research; market analysis; recruit and solicit clients; assess clients' needs and goals; recommend strategies."),
+
+    Careers("Psychology", "Psychologist", "Psychologists study cognitive, emotional, and social processes and behavior by observing, interpreting, and recording how individuals relate to one another and to their environments.",
+        "\$80,980 per year", "Master's or Ph.D.", "Provide assessment and recommendations for appropriate treatments; diagnose and treat various mental, emotional, and behavioral disorders."),
+
+    Careers("Psychology", "Human Resources", "A Human Resources (HR) Officer is responsible for managing every aspect of the employment process, including orientation and training new staff members.",
+        "\$60,262 per year", "Bachelor's", "Recruit candidates; assist with payroll; conduct disciplinary actions; maintain employee records."),
+
+    Careers("Nursing", "Nurse", "A registered nurse, or RN, typically monitors patients, administers medications, keeps records, consults with healthcare providers, educates patients and more.",
+        "\$75,330 per year", "Bachelor's", "Maintaining accurate, complete health care records and reports; administering medications to patients and monitoring them for side effects and reactions; prescribing assistive medical devices and related treatments; recording patient vital signs and medical information."),
+
+    Careers("Nursing", "Nurse Practitioner", "A nurse practitioner (NP) provides primary and specialty healthcare. Like a physician, he or she assesses and diagnoses patients, orders laboratory tests, prescribes medications, and manages health conditions.",
+        "\$111,680 per year", "Master's or Ph.D.", "Ordering, performing and interpreting diagnostic tests such as lab work and x-rays; diagnosing and treating acute and chronic conditions such as diabetes, high blood pressure, infections and injuries; prescribing medications and other treatments; managing patients' overall care."),
+
+    Careers("Engineering", "R&D Engineer", "As R&D Engineer, you will be responsible for developing and improving a wide range of production processes.",
+        "\$92,409 per year", "Bachelor's", "Develop new products; redesign existing products; perform research and testing on product concepts."),
+
+    Careers("Engineering", "Mechanical Engineer", "Mechanical engineers develop, design, build, test, and inspect mechanical devices and systems, such as machines, tools, and engines.",
+        "\$90,160 per year", "Bachelor's", "Research, design, develop, build, and test mechanical and thermal sensors and devices, including tools, engines, and machines."),
+
+    Careers("Engineering", "Engineering Manager", "Engineering Managers design, implement, and refine product development, testing, and manufacturing processes.",
+        "\$138,342 per year", "Bachelor's or Master's", "Designs, implements, and refines product development, testing, and manufacturing processes; manages staff and assigns tasks to engineering and technical personnel; leads teams in the development of new products, solutions, and processes."),
+
+    Careers("Education", "Education Administration", "An educational administrator ensures a safe and productive learning environment for the students and faculty at their institution.",
+        "\$95,000 per year", "Bachelor's or Master's", "Setting and enforcing school budgets; maintaining academic records; assisting students and faculty; managing staff."),
+
+    Careers("Education", "Teacher", "A Teacher is a professional who teaches students based on national curriculum guidelines within their specialist subject areas.",
+        "\$60,320 per year", "Bachelor's or Master's", "Assigning homework; grading tests; documenting progress; keeping up with parent communication."),
+
+    Careers("Education", "Career Advisor", "A Careers Advisor helps people make realistic choices about their education, training and work by providing the right advice, information and guidance.",
+        "\$43,504 per year", "Bachelor's or Master's", "Use aptitude and achievement assessments to help students or clients evaluate their interests, skills, and abilities; evaluate students' or clients' background, education, and training, to help them develop realistic goals."),
+
+    Careers("Law/Political Science", "Lawyer", "Lawyers, also known as attorneys, are certified professionals who advise and represent natural and juristic persons in legal matters.",
+        "\$144,230 per year", "Master's", "Advise and represent clients in courts, before government agencies, and in private legal matters; communicate with their clients, colleagues, judges, and others involved in the case."),
+
+    Careers("Law/Political Science", "Politician", "A politician is a person active in party politics, or a person holding or seeking an elected office in government.",
+        "\$83,137 per year", "Bachelor's or Master's", "Propose, support, reject, and create laws that govern the land and, by extension, its people."),
+
+    Careers("Law/Political Science", "Policy Analyst", "A policy analyst investigates current and potential policies, using their research findings to propose solutions to social issues.",
+        "\$77,719 per year", "Bachelor's or Master's", "Reviewing and amending policy drafts and proposing suggestions to improve the effects of existing policies."),
+
+    Careers("Art/Design", "Illustrator", "As an illustrator, you'll work to commercial briefs to inform, persuade or entertain a client's intended audience, adjusting the mood and style of images accordingly.",
+        "\$49,405 per year", "High School Diploma", "Combining hand-drawing and painting with digital media to create complete illustrations; refining designs with illustration software; using various colors, graphics and effects to better convey each concept."),
+
+    Careers("Art/Design", "Photographer", "Photographers record events and tells stories using images.",
+        "\$67,204 per year", "High School Diploma", "Market or advertise services to attract clients; analyze and plan the composition of photographs; use various photographic techniques and lighting equipment."),
+
+    Careers("Art/Design", "Graphic Designer", "Graphic designers create visual text and imagery concepts, by hand or using computer software, to communicate ideas that inspire, inform, or captivate consumers.",
+        "\$47,334 per year", "Bachelor's", "Create and design various materials for print and digital collateral; ensure projects are completed with high quality and on schedule; establish creative direction for the company as well as brand guidelines"),
+
+    Careers("Math/Statistics", "Actuary", "Actuaries analyze the financial costs of risk and uncertainty.",
+        "\$111,030 per year", "Bachelor's", "Collect and compile statistical data for additional analysis; estimate probability and likely cost of events such as an accident, death, natural disaster or sickness; design, test, and administer policies to minimize risk."),
+
+    Careers("Math/Statistics", "Financial Manager", "Financial managers perform data analysis and advise senior managers on profit-maximizing ideas.",
+        "\$134,180 per year", "Bachelor's", "Prepare financial statements, business activity reports, and forecasts; monitor financial details to ensure that legal requirements are met."),
+
+    Careers("Math/Statistics", "Data Scientist", "Data scientists utilize their analytical, statistical, and programming skills to collect, analyze, and interpret large data sets.",
+        "\$100,560", "Master's or Ph.D.", "Ask the right questions to begin the discovery process; acquire data; process and clean the data; integrate and store data."),
+
+    Careers("Environmental Science", "Environmental Engineer", "Environmental engineers are problem-solvers who find innovative solutions to environmental issues.",
+        "\$92,120", "Bachelor's", "Collecting and analyzing environmental data, studying human influences on the environment, and improving environmental conservation management."),
+
+    Careers("Environmental Science", "Wildlife biologist", "Wildlife biologists study animals in their natural habitats to understand their environments.",
+        "\$50,186", "Bachelor's", "Making recommendations about animal habitat and care and planning for wildlife environments; estimating plant and animal populations; classifying animals based on genetic data taken from animals."),
+
+    Careers("Environmental Science", "Occupational Health & Safety Specialist", "Occupational health and safety specialists examine the workplace for environmental or physical factors that could affect employee health, safety, comfort, and performance.",
+        "\$72,530", "Bachelor's", "Inspect, test, and evaluate workplace environments, equipment, and practices to ensure that they follow safety standards and government regulations."),
+
+  ];
+
+
+
+  String major;
+  String careers;
+  String description;
+  String salary;
+  String education;
+  String responsibilities;
+
+  //List<String> images;
+
+  Careers(this.major, this.careers, this.description, this.salary, this.education, this.responsibilities);
+}
